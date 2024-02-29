4fc39f7b3cd3167bcc8ee28995fcf0adb6c22828
Rin Concordia <rin@skylight.digital>
2024-02-28 19:44:55 -0600

    Add foundational templates and environment configs.

---
+++ templates/foundations/main.tf
@@ -0,0 +1,37 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "${team}-${project}-${env}-rg"
+  location = "${location}"
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                        = "${team}${project}${env}kv"
+  location                    = var.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+}
+
+resource "azurerm_container_registry" "sr" {
+  location            = data.azurerm_resource_group.rg.location
+  name                = "${team}${project}${env}acr"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  sku                 = "Standard"
+  admin_enabled       = true
+}
+
+resource "azurerm_storage_account" "app" {
+  account_replication_type         = "GRS" # Cross-regional redundancy
+  account_tier                     = "Standard"
+  account_kind                     = "StorageV2"
+  name                             = "${team}${project}${env}sa"
+  resource_group_name              = data.azurerm_resource_group.rg.name
+  location                         = data.azurerm_resource_group.rg.location
+  enable_https_traffic_only        = true
+  min_tls_version                  = "TLS1_2"
+  allow_nested_items_to_be_public  = false
+  cross_tenant_replication_enabled = false
+}
