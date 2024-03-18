8401e4afc7507d0a0d4a7cf8de663fe3ac9f126e
Rin Concordia <rin@skylight.digital>
2024-03-18 16:27:13 -0500

    Finalize App Gateway implementation and templating.

--- ops/resources/virtual_network/_var.tf
+++ ops/resources/virtual_network/_var.tf
@@ -1,21 +1,21 @@
 variable "team" {
   description = "One-word identifier for this project's custodial team."
-  type        = string
+  type = string
 }
 
 variable "project" {
   description = "One-word identifier or code name for this project."
-  type        = string
+  type = string
 }
 
 variable "env" {
   description = "One-word identifier for the target environment (e.g. dev, test, prod)."
-  type        = string
+  type = string
 }
 
 variable "location" {
   description = "The Azure region in which the associated resources will be created."
-  type        = string
+  type = string
 }
 
 variable "resource_group_name" {
@@ -23,6 +23,38 @@ variable "resource_group_name" {
   type        = string
 }
 
-variable "network_address" {
-  description = "The network address of the virtual network"
+variable "network_address_space" {
+  description = "The desired address space for the full virtual network"
+  type = string
+  default = "10.30.0.0/16"
+}
+
+variable "k8s_subnet_address_prefix" {
+  type        = string
+  description = "IP address space for kubernetes subnet vnet"
+  default     = "10.30.1.0/24"
+}
+
+variable "app_gateway_subnet_address_prefix" {
+  type        = string
+  description = "App gateway subnet server IP address space."
+  default     = "10.30.5.0/24"
+}
+
+variable "lb_subnet_address_prefix" {
+  type        = string
+  description = "Load balancer subnet IP address space."
+  default     = "10.30.7.0/24"
+}
+
+variable "app_gateway_sku" {
+  type        = string
+  description = "Name of the Application Gateway SKU"
+  default     = "Standard_v2"
+}
+
+variable "app_gateway_tier" {
+  type        = string
+  description = "Tier of the Application Gateway tier"
+  default     = "Standard_v2"
 }
---
+++ templates/virtual_network/gateway.tf
@@ -0,0 +1,85 @@
+locals {
+  backend_address_pool_name      = "${azurerm_virtual_network.vnet.name}-beap"
+  frontend_port_name             = "${azurerm_virtual_network.vnet.name}-feport"
+  frontend_ip_configuration_name = "${azurerm_virtual_network.vnet.name}-feip"
+  http_setting_name              = "${azurerm_virtual_network.vnet.name}-be-htst"
+  listener_name                  = "${azurerm_virtual_network.vnet.name}-httplstn"
+  request_routing_rule_name      = "${azurerm_virtual_network.vnet.name}-rqrt"
+
+  #networkContributorRole         = "[concat('/subscriptions/', subscription().subscriptionId, '/providers/Microsoft.Authorization/roleDefinitions/', '4d97b98b-1d4f-4787-a291-c67834d212e7')]"
+
+  app_gateway_subnet_name = "appgwsubnet"
+}
+
+resource "azurerm_application_gateway" "k8s" {
+  name                = "${local.name}-k8s-gateway"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+
+  sku {
+    name     = var.app_gateway_sku
+    tier     = "Standard_v2"
+    capacity = 2
+  }
+
+  gateway_ip_configuration {
+    name      = "appGatewayIpConfig"
+    subnet_id = azurerm_subnet.appgw.id
+  }
+
+  frontend_port {
+    name = local.frontend_port_name
+    port = 80
+  }
+
+  frontend_port {
+    name = "httpsPort"
+    port = 443
+  }
+
+  frontend_ip_configuration {
+    name                 = local.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.k8s_ingress.id
+  }
+
+  backend_address_pool {
+    name = local.backend_address_pool_name
+  }
+
+  backend_http_settings {
+    name                  = local.http_setting_name
+    cookie_based_affinity = "Disabled"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 1
+  }
+
+  http_listener {
+    name                           = local.listener_name
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
+    frontend_port_name             = local.frontend_port_name
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = local.request_routing_rule_name
+    rule_type                  = "Basic"
+    http_listener_name         = local.listener_name
+    backend_address_pool_name  = local.backend_address_pool_name
+    backend_http_settings_name = local.http_setting_name
+    priority = 10
+  }
+
+  depends_on = [
+    azurerm_virtual_network.vnet,
+    azurerm_public_ip.k8s_ingress
+  ]
+}
+
+resource "azurerm_public_ip" "k8s_ingress" {
+  name                         = "${local.name}-k8s-gateway-ip"
+  location                     = data.azurerm_resource_group.rg.location
+  resource_group_name          = data.azurerm_resource_group.rg.name
+  allocation_method            = "Static"
+  sku                          = "Standard"
+}
