d67814ea406d938607e2bfa4a533f70da5249692
marycrawford <mary.crawford@skylight.digital>
2024-03-25 14:05:48 -0700

    initial commit for k8s/aks template

---
+++ templates/k8/main.tf
@@ -0,0 +1,45 @@
+# Generate resource group 
+resource "aks_tf_rg" "resource_group_name" {
+  prefix = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = aks_tf_rg.resource_group_name.id
+}
+
+resource "aks_tf_rg" "azurerm_kubernetes_cluster_name" {
+  prefix = "cluster"
+}
+
+resource "aks_tf_rg" "azurerm_kubernetes_cluster_dns_prefix" {
+  prefix = "dns"
+}
+
+resource "azurerm_kubernetes_cluster" "k8s" {
+  location            = azurerm_resource_group.rg.location
+  name                = aks_tf_rg.azurerm_kubernetes_cluster_name.id
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = aks_tf_rg.azurerm_kubernetes_cluster_dns_prefix.id
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  default_node_pool {
+    name       = "agentpool"
+    vm_size    = "Standard_D2_v2"
+    node_count = var.node_count
+  }
+  linux_profile {
+    admin_username = var.username
+
+    ssh_key {
+      key_data = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
+    }
+  }
+  network_profile {
+    network_plugin    = "kubenet"
+    load_balancer_sku = "standard"
+  }
+}
