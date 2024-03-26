618d1d0c284adf29d128ca7c4ff501302f37be5d
marycrawford <mary.crawford@skylight.digital>
2024-03-26 14:21:33 -0700

    add aks module to ops/resources folder

---
+++ ops/resources/aks/variables.tf
@@ -0,0 +1,23 @@
+variable "resource_group_location" {
+  type        = string
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+variable "node_count" {
+  type        = number
+  description = "The initial quantity of nodes for the node pool."
+  default     = 3
+}
+
+variable "msi_id" {
+  type        = string
+  description = "The Managed Service Identity ID. Set this value if you're running this example using Managed Identity as the authentication method."
+  default     = null
+}
+
+variable "username" {
+  type        = string
+  description = "The admin username for the new cluster."
+  default     = "azureadmin"
+}
