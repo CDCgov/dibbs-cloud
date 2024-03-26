ea86043c8dbaf2227795a38bd8894106cb209738
marycrawford <mary.crawford@skylight.digital>
2024-03-26 14:30:06 -0700

    add variables to aks module

--- ops/resources/aks/variables.tf
+++ ops/resources/aks/variables.tf
@@ -21,3 +21,23 @@ variable "username" {
   description = "The admin username for the new cluster."
   default     = "azureadmin"
 }
+
+variable "team" {
+  description = "One-word identifier for this project's custodial team."
+  type        = string
+}
+
+variable "project" {
+  description = "One-word identifier or code name for this project."
+  type        = string
+}
+
+variable "env" {
+  description = "One-word identifier for the target environment (e.g. dev, test, prod)."
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region in which the associated resources will be created."
+  type        = string
+}
