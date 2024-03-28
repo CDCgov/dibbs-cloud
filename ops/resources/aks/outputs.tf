7726f6eebef5acd88569ebfd3d9668247ead5881
marycrawford <mary.crawford@skylight.digital>
2024-03-28 12:56:38 -0700

    add key_data in output file in /ops folder

--- ops/resources/aks/outputs.tf
+++ ops/resources/aks/outputs.tf
@@ -36,3 +36,7 @@ output "kube_config" {
   value     = azurerm_kubernetes_cluster.k8s.kube_config_raw
   sensitive = true
 }
+
+output "key_data" {
+  value = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
+}
