046139cb69fc60a2a2fa6960f0d9fd82bb58579f
marycrawford <mary.crawford@skylight.digital>
2024-03-28 14:54:04 -0700

    update aks and templates to use azapi

--- ops/resources/aks/main.tf
+++ ops/resources/aks/main.tf
@@ -17,12 +17,28 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     admin_username = var.username
 
     ssh_key {
-      # key_data = jsondecode(azurerm_ssh_public_key.ssh_public_key_gen.output).publicKey
-      key_data = var.public_key
+      key_data = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
     }
   }
   network_profile {
     network_plugin    = "kubenet"
     load_balancer_sku = "standard"
   }
+}
+
+# SSH Key
+resource "azapi_resource_action" "ssh_public_key_gen" {
+  type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
+  resource_id = azapi_resource.ssh_public_key.id
+  action      = "generateKeyPair"
+  method      = "POST"
+
+  response_export_values = ["publicKey", "privateKey"]
+}
+
+resource "azapi_resource" "ssh_public_key" {
+  type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
+  name      = "phdi-playground-${terraform.workspace}-ssh-key"
+  location  = var.location
+  parent_id = data.azurerm_resource_group.rg.id
 }
--- templates/aks/main.tf
+++ templates/aks/main.tf
@@ -17,7 +17,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     admin_username = var.username
 
     ssh_key {
-      key_data = jsondecode(azurerm_ssh_public_key.ssh_public_key_gen.output).publicKey
+      key_data = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
     }
   }
   network_profile {
@@ -26,9 +26,19 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   }
 }
 
-resource "azurerm_ssh_public_key" "ssh_public_key_gen" {
-  name                = var.name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  public_key          = var.public_key
+# SSH Key
+resource "azapi_resource_action" "ssh_public_key_gen" {
+  type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
+  resource_id = azapi_resource.ssh_public_key.id
+  action      = "generateKeyPair"
+  method      = "POST"
+
+  response_export_values = ["publicKey", "privateKey"]
+}
+
+resource "azapi_resource" "ssh_public_key" {
+  type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
+  name      = "phdi-playground-${terraform.workspace}-ssh-key"
+  location  = var.location
+  parent_id = data.azurerm_resource_group.rg.id
 }
