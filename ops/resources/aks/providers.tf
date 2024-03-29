bc7ded39f1a3fc766bc5ba63d12dc51b9e1f93fc
marycrawford <mary.crawford@skylight.digital>
2024-03-29 10:54:03 -0700

    remove azure subscription id and client id information from providers.tf

--- ops/resources/aks/providers.tf
+++ ops/resources/aks/providers.tf
@@ -10,11 +10,4 @@ terraform {
 provider "azurerm" {
   features {}
 
-
-  #TODO: Ask the team if we should mask these in vault for the templates or just as a github variable.
-  # Vault may add an additional level of security
-  subscription_id = "<azure_subscription_id>"
-  tenant_id       = "<azure_subscription_tenant_id>"
-  client_id       = "<service_principal_appid>"
-  client_secret   = "<service_principal_password>"
 }
