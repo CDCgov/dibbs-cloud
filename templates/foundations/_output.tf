8a761aa6e43afd993e0361f0400238545bc58ed2
marycrawford <mary.crawford@skylight.digital>
2024-04-03 22:10:29 -0700

    update azurerm_resource_group reference for foundations and add data.tf file

--- templates/foundations/_output.tf
+++ templates/foundations/_output.tf
@@ -14,11 +14,11 @@ output "acr_name" {
   value = azurerm_container_registry.acr.name
 }
 
-output "acr_admin_username" {
-  value = azurerm_container_registry.acr.admin_username
-}
+#output "acr_admin_username" {
+#  value = azurerm_container_registry.acr.admin_username
+#}
 
-output "acr_admin_password" {
-  value     = azurerm_container_registry.acr.admin_password
-  sensitive = true
-}
+#output "acr_admin_password" {
+#  value     = azurerm_container_registry.acr.admin_password
+#  sensitive = true
+#}
--- templates/foundations/providers.tf
+++ templates/foundations/providers.tf
@@ -10,4 +10,4 @@ terraform {
 provider "azurerm" {
   features {}
 
-}
+} 
