1972432b204a5bf11eb3b71f775a1d8da49c905a
marycrawford <mary.crawford@skylight.digital>
2024-04-03 22:21:12 -0700

    add resource_group_name to the foundations module declaration

--- ops/dev/main.tf
+++ ops/dev/main.tf
@@ -11,6 +11,7 @@ module "foundations" {
   project  = local.project
   env      = local.env
   location = local.location
+  resource_group_name = module.foundations.resource_group_name
 }
 
 module "virtual_network" {
