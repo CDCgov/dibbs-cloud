6306af50297f557125aca92c7796b0b7d548cd2f
Rin Concordia <rin@skylight.digital>
2024-04-11 12:33:33 -0500

    Correct botched template merge.

--- templates/implementation/main.tf
+++ templates/implementation/main.tf
@@ -42,37 +42,6 @@ module "app_service" {
   depends_on = [module.sql_server]
 }
 
-
-module "sql_server" {
-  source              = "../resources/sql_database"
-  team                = local.team
-  project             = local.project
-  env                 = local.env
-  location            = local.location
-  resource_group_name = module.foundations.resource_group_name
-  network_address     = "x.x.x.x/xx"
-}
-
-module "app_service" {
-  source   = "../resources/app_service"
-  team     = local.team
-  project  = local.project
-  env      = local.env
-  location = local.location
-
-  resource_group_name = local.resource_group_name
-
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  key_vault_id = module.foundations.key_vault_id
-
-  webapp_subnet_id = module.virtual_network.subnet_lbs_id
-
-  // If your service has a database dependency, use the following meta argument to
-  // ensure the database is created before the service.
-  depends_on = [module.sql_server]
-}
-
-
 module "sql_server" {
   source   = "../resources/sql_database"
   team     = local.team
@@ -82,7 +51,7 @@ module "sql_server" {
 
   resource_group_name = local.resource_group_name
   global_vault_id     = module.foundations.key_vault_id
-  administrator_login = "octopus_admin"
+  administrator_login = "admin"
   webapp_subnet_id    = module.virtual_network.subnet_lbs_id
 }
 
