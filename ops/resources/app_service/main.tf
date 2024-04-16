a9a55eb9567807023035d2dca8eb2e218c609195
Rin Concordia <rin@skylight.digital>
2024-04-16 18:52:01 -0500

    Correct octopus server tag.

--- ops/resources/app_service/main.tf
+++ ops/resources/app_service/main.tf
@@ -35,8 +35,8 @@ resource "azurerm_linux_web_app" "octopus" {
 
   site_config {
     application_stack {
-      docker_image_name   = "${var.acr_url}/dibbs-cloud/octopusdeploy:${var.octopus_image_version}"
-      docker_registry_url = "https://index.docker.io"
+      docker_image_name   = "dibbs-cloud/octopusdeploy:${var.octopus_image_version}"
+      docker_registry_url = "https://${var.acr_url}"
     }
   }
 
