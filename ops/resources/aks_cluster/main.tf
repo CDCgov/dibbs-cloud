6f3501b777109c449769250324d89c64e5f206e2
shanice-skylight <shanice@skylight.digital>
2024-07-10 22:42:10 -0400

    fixed fmt check by using terraform fmt -check -recursive

--- ops/resources/aks_cluster/main.tf
+++ ops/resources/aks_cluster/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   dns_prefix          = local.name
 
   azure_policy_enabled = true
-#based ths ip off of the aks_service_cidr, this value should Limit the access to the API server to a limited IP range
+  #based ths ip off of the aks_service_cidr, this value should Limit the access to the API server to a limited IP range
   api_server_access_profile {
     authorized_ip_ranges = [
       "10.0.0.0/16"
